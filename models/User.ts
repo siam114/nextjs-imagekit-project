1030b2b1ebb898b592549fb721c1d9f5e3690ceb
siam114 <smsiamislam598@gmail.com>
2025-06-25 17:49:29 +0600

    bcrypt password

--- models/User.ts
+++ models/User.ts
@@ -14,5 +14,16 @@ const userSchema = new Schema<IUser>(
     {
         email: {type: String, required: true, unique: true},
         password: {type: String, required: true},
+    },
+    {
+        timestamps: true,
     }
-)
+);
+
+userSchema.pre('save', async function(next) {
+    if(this.isModified('password')){
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+    
+})
