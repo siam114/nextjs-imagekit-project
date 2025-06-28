284b0913d54e6ab42cf80e61e286636fd4885961
siam114 <smsiamislam598@gmail.com>
2025-06-28 16:44:06 +0600

    email and pass validation

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -2,3 +2,16 @@ import { connectionToDatabase } from "@/lib/db"
 import User from "@/models/User"
 import { NextRequest, NextResponse } from "next/server"
 
+
+export async function POST(request: NextRequest) {
+    try {
+        const {email, password} = await request.json()
+
+         if(!email || !password) {
+            return NextResponse.json({error: "Email and password are required"}, {status: 400})
+         }
+
+    }catch (error) {
+
+    }
+} 
