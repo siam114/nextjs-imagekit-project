92e47223846371c9ed9387525ee3bbe96dd63f24
siam114 <smsiamislam598@gmail.com>
2025-06-28 16:53:02 +0600

    existing user

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,4 +1,4 @@
-import { connectionToDatabase } from "@/lib/db"
+import { connectToDatabase } from "@/lib/db"
 import User from "@/models/User"
 import { NextRequest, NextResponse } from "next/server"
 
@@ -10,6 +10,16 @@ export async function POST(request: NextRequest) {
          if(!email || !password) {
             return NextResponse.json({error: "Email and password are required"}, {status: 400})
          }
+
+        await connectToDatabase();
+
+        const existingUser = await User.findOne({ email });
+        if( existingUser) {
+            return NextResponse.json(
+                {error: "User already exists"},
+                {status: 400}
+            )
+        }
 
     }catch (error) {
 
