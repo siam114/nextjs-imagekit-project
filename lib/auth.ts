304fde2cb5c50a009b2b09c4b79608e7e4c49787
siam114 <smsiamislam598@gmail.com>
2025-06-30 15:45:16 +0600

    create credentials

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,18 @@
 import { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions:NextAuthOptions = {
     providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID!,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-        })
+       CredentialsProvider({
+        name: "Credentials",
+        credentials: {
+              email: {label: "Email", type: "text"},
+              password: {label: "Password", type: "password"}
+        },
+
+        async authorize(credentials) {
+
+        }
+       })
     ]
 }
