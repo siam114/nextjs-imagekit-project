b323b2412a39b61bd46578777af52475b7df6e9c
siam114 <smsiamislam598@gmail.com>
2025-07-01 18:22:56 +0600

    added page session and secret

--- lib/auth.ts
+++ lib/auth.ts
@@ -55,12 +55,23 @@ export const authOptions:NextAuthOptions = {
             }
             return token;
         },
-        
+
         async session({ session, token }) {
             if(session.user) {
                 session.user.id = token.id as string;
             }
             return session;
         }
-    }
+    },
+
+    pages: {
+        signIn: "/login",
+        error: "/login",
+    },
+
+    session: {
+        strategy: "jwt",
+        maxAge: 30 * 24 * 60 * 60, // 30 days
+    },
+    secret: process.env.NEXTAUTH_SECRET,
 }
