d125cd35ffc12781f2c2c7c340c6ca031ac71d32
siam114 <smsiamislam598@gmail.com>
2025-07-02 22:08:30 +0600

    set authentication

--- app/api/imagekit-auth/route.ts
+++ app/api/imagekit-auth/route.ts
@@ -2,10 +2,10 @@ import { getUploadAuthParams } from "@imagekit/next/server"
 
 export async function GET() {
 
-    const { token, expire, signature } = getUploadAuthParams({
+    const authenticationParameters = getUploadAuthParams({
         privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
-        publicKey: process.env.IMAGEKIT_PUBLIC_KEY as string,
+        publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
     })
 
-    return Response.json({ token, expire, signature, publicKey: process.env.IMAGEKIT_PUBLIC_KEY })
+    return Response.json({ authenticationParameters, publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY })
 }
