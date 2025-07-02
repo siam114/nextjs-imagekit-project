a6980914a37f7090a7b10ec137963aca07d59b2b
siam114 <smsiamislam598@gmail.com>
2025-07-02 22:19:06 +0600

    use try and catch

--- app/api/imagekit-auth/route.ts
+++ app/api/imagekit-auth/route.ts
@@ -2,10 +2,15 @@ import { getUploadAuthParams } from "@imagekit/next/server"
 
 export async function GET() {
 
-    const authenticationParameters = getUploadAuthParams({
+   try {
+     const authenticationParameters = getUploadAuthParams({
         privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
         publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
     })
 
     return Response.json({ authenticationParameters, publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY })
+   }catch (error) {
+    console.error("Error generating authentication parameters:", error);
+    // return Response.json({ error: "Failed to generate authentication parameters" }, { status: 500 });
+   }
 }
