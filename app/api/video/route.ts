752b5bc2ad45d79f817b6adbf39028d14fe2c8f2
siam114 <smsiamislam598@gmail.com>
2025-07-03 23:29:31 +0600

    sorto check

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,10 +1,16 @@
 import { connectToDatabase } from "@/lib/db";
 import Video from "@/models/Video";
+import { NextResponse } from "next/server";
 
 export async function GET(){
     try {
         await connectToDatabase();
-        const videos = Video.find().sort({ createdAt: -1 }).lean();
+        const videos = await Video.find().sort({ createdAt: -1 }).lean();
+
+        if(!videos || videos.length === 0) {
+            return NextResponse.json([], { status: 200 });
+        }
+
     }catch (error) {
         console.error("Database connection failed:", error);
     }
