ec43ac0b2ea73054a5947deb1cf4df9bb9402f81
siam114 <smsiamislam598@gmail.com>
2025-07-03 23:26:20 +0600

    find video

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,8 +1,10 @@
 import { connectToDatabase } from "@/lib/db";
+import Video from "@/models/Video";
 
 export async function GET(){
     try {
         await connectToDatabase();
+        const videos = Video.find().sort({ createdAt: -1 }).lean();
     }catch (error) {
         console.error("Database connection failed:", error);
     }
