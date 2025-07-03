ddbc14565c0e40677b39a77c7d82ed30d6904355
siam114 <smsiamislam598@gmail.com>
2025-07-03 15:05:56 +0600

    formating

--- app/api/imagekit-auth/route.ts
+++ app/api/imagekit-auth/route.ts
@@ -1,16 +1,21 @@
-import { getUploadAuthParams } from "@imagekit/next/server"
+import { getUploadAuthParams } from "@imagekit/next/server";
 
 export async function GET() {
+  try {
+    const authenticationParameters = getUploadAuthParams({
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
+      publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
+    });
 
-   try {
-     const authenticationParameters = getUploadAuthParams({
-        privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
-        publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
-    })
-
-    return Response.json({ authenticationParameters, publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY })
-   }catch (error) {
+    return Response.json({
+      authenticationParameters,
+      publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+    });
+  } catch (error) {
     console.error("Error generating authentication parameters:", error);
-    return Response.json({ error: "Failed to generate authentication parameters" }, { status: 500 });
-   }
+    return Response.json(
+      { error: "Failed to generate authentication parameters" },
+      { status: 500 }
+    );
+  }
 }
