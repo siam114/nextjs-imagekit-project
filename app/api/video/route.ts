25065599d1edbaaed74e26abe7502493e46c2476
siam114 <smsiamislam598@gmail.com>
2025-07-04 15:42:41 +0600

    formating

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -2,22 +2,21 @@ import { connectToDatabase } from "@/lib/db";
 import Video from "@/models/Video";
 import { NextResponse } from "next/server";
 
-export async function GET(){
-    try {
-        await connectToDatabase();
-        const videos = await Video.find().sort({ createdAt: -1 }).lean();
+export async function GET() {
+  try {
+    await connectToDatabase();
+    const videos = await Video.find().sort({ createdAt: -1 }).lean();
 
-        if(!videos || videos.length === 0) {
-            return NextResponse.json([], { status: 200 });
-        }
-
-        return NextResponse.json(videos);
-
-    }catch (error) {
-        console.error("Error fetching videos:", error);
-        return NextResponse.json(
-            { error: "Failed to fetch videos",},
-            { status: 500 }
-        );
+    if (!videos || videos.length === 0) {
+      return NextResponse.json([], { status: 200 });
     }
+
+    return NextResponse.json(videos);
+  } catch (error) {
+    console.error("Error fetching videos:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch videos" },
+      { status: 500 }
+    );
+  }
 }
