e07872629aef486f0ca56d612a4c1b8595f4bc1f
siam114 <smsiamislam598@gmail.com>
2025-07-06 16:40:24 +0600

    usestate

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 
 function RegisterPage() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
   return (
     <div>
       register page
