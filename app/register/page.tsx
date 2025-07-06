616bd72092172a694a939e3e489495698b29ab7c
siam114 <smsiamislam598@gmail.com>
2025-07-06 16:58:35 +0600

    fetching data

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -9,12 +9,31 @@ function RegisterPage() {
     const router = useRouter();
 
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if (password !== confirmPassword) {
             alert("Passwords do not match");
             return;
         }
+
+        try {
+
+          const res = await fetch('/api/auth/register', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+          })
+
+          const data = await res.json();
+          
+
+        }catch (error){
+
+        }
+
+
     }
 
   return (
