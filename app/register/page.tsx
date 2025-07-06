0e47cad91a2c7ab9ee3caa2c71eb984ccac2e2b2
siam114 <smsiamislam598@gmail.com>
2025-07-06 16:44:17 +0600

    userouter navigation

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
 
 function RegisterPage() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
+    const router = useRouter();
+    
   return (
     <div>
       register page
