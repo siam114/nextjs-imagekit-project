2b191cb12f452b1f248a0225959b6c86d53873cc
siam114 <smsiamislam598@gmail.com>
2025-07-06 20:08:30 +0600

    formating register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,44 +1,41 @@
 "use client";
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react'
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 function RegisterPage() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
-    const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const router = useRouter();
 
-
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (password !== confirmPassword) {
-            alert("Passwords do not match");
-            return;
-        }
-
-        try {
-
-          const res = await fetch('/api/auth/register', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email, password }),
-          })
-
-          const data = await res.json();
-
-          if( !res.ok ){
-            throw new Error(data.error || 'Registration failed');
-          }
-
-          console.log(data);
-          router.push('/login');
-
-        }catch (error){
-          console.error(error);
-        }
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
     }
+
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.error || "Registration failed");
+      }
+
+      console.log(data);
+      router.push("/login");
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div>
@@ -57,17 +54,17 @@ function RegisterPage() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
-          />
-          <input
-           type='password'
-           placeholder='Confirm Password'
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
       </form>
     </div>
-  )
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
