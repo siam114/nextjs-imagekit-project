a23bb21ba0b2988df5b4fd6385b046e79a1705e8
siam114 <smsiamislam598@gmail.com>
2025-07-06 16:46:51 +0600

    handleSubmit

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -7,7 +7,12 @@ function RegisterPage() {
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
     const router = useRouter();
-    
+
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }
+
   return (
     <div>
       register page
