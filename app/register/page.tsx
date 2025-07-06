5928222e6736587fb0e02e380e22f81b761238dd
siam114 <smsiamislam598@gmail.com>
2025-07-06 20:07:23 +0600

    set email password

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -42,7 +42,30 @@ function RegisterPage() {
 
   return (
     <div>
-      register page
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          />
+          <input
+           type='password'
+           placeholder='Confirm Password'
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+      </form>
     </div>
   )
 }
