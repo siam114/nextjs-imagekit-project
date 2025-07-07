e9c758d43ef8a85cbf997766183a53ebc1e46c12
siam114 <smsiamislam598@gmail.com>
2025-07-07 23:44:41 +0600

    create handlrsubmit function

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,11 @@ const LoginPage = () => {
     const [email, setEmail] =useState("");
     const [password, setPassword] =useState("");
     const router = useRouter();
-    
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }
+
   return (
     <div>
       login page
