22b9bda95c7976c3e67c2dabefd873e5770c46f9
siam114 <smsiamislam598@gmail.com>
2025-07-07 23:28:15 +0600

    set email and pass

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+'use client';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
 
 const LoginPage = () => {
+    const [email, setEmail] =useState("");
+    const [password, setPassword] =useState("");
+    const router = useRouter();
+    
   return (
     <div>
       login page
