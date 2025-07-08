bb813ba693ff03a6bfd7b9275a3e1dda6f837ea2
siam114 <smsiamislam598@gmail.com>
2025-07-09 01:38:58 +0600

    create middleware

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,32 @@
-import { withAuth } from "next-auth/middleware"
-import { NextResponse } from "next/server"
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
 export default withAuth(
   function middleware() {
-    return NextResponse.next()
+    return NextResponse.next();
   },
   {
     callbacks: {
-      authorized({req, token}) {
-        const { pathname } = req.nextUrl
-        if(
+      authorized({ req, token }) {
+        const { pathname } = req.nextUrl;
+        if (
           pathname.startsWith("/api/auth") ||
           pathname === "/login" ||
           pathname === "/register"
         )
-        return true;
+          return true;
 
-        if(pathname === "/" || pathname.startsWith("/api/videos")) {
+        if (pathname === "/" || pathname.startsWith("/api/videos")) {
           return true;
         }
 
         return !!token;
-      }
-    }
+      },
+    },
   }
-)
+);
 
 export const config = {
   // Match all paths except for the ones specified
-  matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|login|error).*)",
-  ],
-}
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|login|error).*)"],
+};
