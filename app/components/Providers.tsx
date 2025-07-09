d2ade0cae05caca0e179fda89500151e8d2d69ff
siam114 <smsiamislam598@gmail.com>
2025-07-09 21:21:59 +0600

    sessionprovider

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -1,6 +1,8 @@
+import { SessionProvider } from "next-auth/react";
+
 export default function Providers({ children }: { children: React.ReactNode }) {
     return
-        <>
+        <SessionProvider refetchInterval={5 * 60}>
         {children}
-        </>;
+        </SessionProvider>;
 }
